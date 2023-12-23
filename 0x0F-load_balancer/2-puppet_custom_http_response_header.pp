a9b50518eeda2bd6343bb80d0710096bf6ac2e58
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-12-23 16:44:32 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,35 @@
 # puppet manifest creating a custom HTTP header response
-
-exec {'update_system':
-  command => '/usr/bin/apt-get -y update',
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure => 'installed',
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   require => Package['nginx'],
 }
 
-file { '/var/www/html/index.html' :
-  content => 'Hello World!',
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
-file_line { 'Creating header':
-  	ensure => present,
-  	path   => '/etc/nginx/sites-available/default',
-  	line   => "\tadd_header X-Served-By ${hostname};",
-  	after  => 'server_name _;',
-	require => Service['nginx'],
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
